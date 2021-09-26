ecdaeb955fed8a29af76c8bd5ea46be0b08c6c90
harshityagi <harshit97tyagi@gmail.com>
2021-09-26 23:49:55 +0530

    First Commit to codePado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Devi Courier</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Hi', age: 29}
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Devi Courier</h1>
+        <p>This is a Great Courier Website.</p>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+      </div>
+    );
+    //return React.createElement('div',null,'h1', "Devi");
+  }
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const person = (props) => {
+	return (
+        <div>
+        <p>{props.name} is very hard working at {props.age} years of age!!</p>
+        </div>
+    )    
+    };
+
+export default person;
